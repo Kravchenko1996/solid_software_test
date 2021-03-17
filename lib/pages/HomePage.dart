5dcc5f5c73e06742bc86edb04449e24a10b37616
Kravchenko1996 <kravoleksii@gmail.com>
2021-03-17 14:26:16 +0200

    - change randomColor generation;

--- lib/pages/HomePage.dart
+++ lib/pages/HomePage.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -14,6 +16,7 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   Color bgColor = Colors.white;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,22 +27,36 @@ class _HomePageState extends State<HomePage> {
       ),
       body: GestureDetector(
         behavior: HitTestBehavior.translucent,
-        onTap: () {
-          setState(() {
-            bgColor = ([...Colors.primaries]..shuffle()).first;
-          });
-        },
-        child: _buildChild('Hey there'),
+        onTap: () => _changeColor(),
+        child: _buildContent('Hey there'),
       ),
     );
   }
 
-  Widget _buildChild(String text) {
+  void _changeColor() {
+    setState(() {
+      bgColor = _randomColor().withOpacity(1.0);
+    });
+  }
+
+  Color _randomColor() {
+    return Color(Random().nextInt(0xFFFFFF));
+  }
+
+  Widget _buildContent(String text) {
     return Center(
       child: Text(
         text,
         style: TextStyle(
           fontSize: 40,
+          color: bgColor == Colors.white ? Colors.black : Colors.white,
+          shadows: [
+            Shadow(
+              offset: Offset(1, 1),
+              blurRadius: 1,
+              color: Colors.black,
+            )
+          ],
         ),
       ),
     );
